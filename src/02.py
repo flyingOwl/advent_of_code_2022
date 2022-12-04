36b9d470e6abcba2399e60f760ba8fd570d673ee
flyingOwl <ofenfisch@googlemail.com>
2022-12-04 15:40:04 +0100

    Remove redundant .readlines() after open()
    Iterating over open() will provide each line

--- src/02.py
+++ src/02.py
@@ -1,6 +1,6 @@
 import sys
 G=lambda j:[2,0,1,2,0][j:]
-d=[(ord(l[0])-65,ord(l[2])-88) for l in open(sys.argv[1]).readlines()]
+d=[(ord(l[0])-65,ord(l[2])-88) for l in open(sys.argv[1])]
 s=[0,0]
 for a,b in d:
 	s[0]+=1+b+G(a).index(b)*3
