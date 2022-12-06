10e763092f80c8cfbee31f19e369497a190bff26
flyingOwl <ofenfisch@googlemail.com>
2022-12-06 11:47:26 +0100

    Day 06
    Remove lambda and print list comprehension directly, saving 6 bytes

--- src/06.py
+++ src/06.py
@@ -1,4 +1,3 @@
 import sys
 L=open(sys.argv[1]).read()
-F=lambda n:next(c for c in range(n,len(L)) if len(set(L[c-n:c]))==n)
-print([F(4),F(14)])
+print([next(c for c in range(n,len(L)) if len(set(L[c-n:c]))==n) for n in [4,14]])
