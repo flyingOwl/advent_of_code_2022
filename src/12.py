c88533d6ed475f3db31e62b2ed8d67f105da6c6b
flyingOwl <ofenfisch@googlemail.com>
2022-12-12 12:03:03 +0100

    Day 12
    Bugfix: S and E must set to 0 and 26

--- src/12.py
+++ src/12.py
@@ -1,7 +1,7 @@
 import sys
 M={};L={};e=enumerate
 for y,l in e(open(sys.argv[1])):
-	for x,c in e(l[:-1]):p=x+y*1j;L[c]=p;M[p]=ord(c)-[97,83][c<'a']
+	for x,c in e(l[:-1]):p=x+y*1j;L[c]=p;M[p]=[ord(c)-97,26*(c<'S')][c<'a']
 def F(P):
 	s=0;V=P
 	while L['E'] not in P:V|=P;s+=1;P={p+d for p in P for d in [-1,1,-1j,1j] if p+d not in V and M.get(p+d,99)<M[p]+2}
