4dccad9251b9871f406f72715ea68a33c4803baf
flyingOwl <ofenfisch@googlemail.com>
2022-12-13 16:03:57 +0100

    Day 13
    Replace list comprehension with filter(), saving 12 bytes

--- src/13.py
+++ src/13.py
@@ -4,7 +4,7 @@ def C(a,b):
 	if t(a)==t(b)==int:return a-b
 	if t(a)==int:a=[a]
 	if t(b)==int:b=[b]
-	return x if (x:=next((x for z in zip(a,b) if (x:=C(*z))),0)) else len(a)-len(b)
+	return x if (x:=next(filter(bool,map(C,a,b)),0)) else len(a)-len(b)
 L=[eval(l) for l in open(sys.argv[1]) if l.strip()]
 s=sum(1+i//2 for i in range(0,len(L),2) if C(*L[i:i+2])<0)
 x=lambda e,s:sum(C(a,[[e]])<0 for a in L)+s
