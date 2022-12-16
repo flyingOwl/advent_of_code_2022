1b956dfcb0c387dc6caf9c33c2eb6ab665d1a865
flyingOwl <ofenfisch@googlemail.com>
2022-12-17 00:37:54 +0100

    Day 16
    Golfed down to 516 bytes

--- src/16.py
+++ src/16.py
@@ -1,29 +1,7 @@
 import sys
-S={(v:=l.split())[1]:[int(v[4][5:-1]),{w.strip(',') for w in v[9:]}] for l in open(sys.argv[1])}
-def R(S,p):
-	s=1
-	v={p}
-	d={}
-	n=S[p][1]
-	while n:
-		s+=1;v|=n;m=set()
-		for e in n:
-			if S[e][0]:d[e]=s
-			m|=S[e][1]
-		n=m-v
-	return d
-r={p:R(S,p) for p in S}
-P="AA"
-def T(M,p,t,o):
-	Q=r[p]
-	M[tuple(o.keys())]=sum(S[k][0]*v for k,v in o.items())
-	for q in Q.keys()-o.keys():
-		if t>Q[q]:T(M,q,t-Q[q],{q:t-Q[q]}|o)
-M={}
-T(M,P,30,{})
-N={};E={}
-T(N,P,26,{})
-for n in sorted(N,key=N.get):
-	E[tuple(sorted(n))]=N[n]
-MM=max(x+y for e,x in E.items() for h,y in E.items() if not set(h)&set(e))
-print([max(M.values()),MM])
+S={(v:=l.split())[1]:[int(v[4][5:-1]),{w[:2]for w in v[9:]}] for l in open(sys.argv[1])}
+R=lambda S,n,v,t:{e:t+1 for e in n if S[e][0]*t}|R(S,{e for f in n for e in S[f][1]-v},v|n,t+1)if n else{}
+r={p:R(S,{p},set(),0)for p in S}
+def T(M,t,Q=r["AA"],o={}):M[(*o,)]=sum(o.values());[T(M,u,r[q],{q:u*S[q][0]}|o)for q in Q if q not in o and(u:=t-Q[q])>0]
+M={};T(M,26);E={(*sorted(n),):M[n]for n in sorted(M,key=M.get)};T(M,30)
+print([max(M.values()),max(E[e]+E[h]for e in E for h in E if not set(h)&set(e))])
