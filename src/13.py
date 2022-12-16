df8977f58b72d18a5e3e7832157c0946ca9c8250
flyingOwl <ofenfisch@googlemail.com>
2022-12-16 21:14:21 +0100

    Day 13
    Do not wrap e in a list, saving 4 bytes

--- src/13.py
+++ src/13.py
@@ -7,5 +7,5 @@ def C(a,b):
 	return x if (x:=next(filter(bool,map(C,a,b)),0)) else len(a)-len(b)
 L=[eval(l) for l in open(sys.argv[1]) if l.strip()]
 s=sum(1+i//2 for i in range(0,len(L),2) if C(*L[i:i+2])<0)
-x=lambda e,s:sum(C(a,[[e]])<0 for a in L)+s
+x=lambda e,s:sum(C(a,e)<0 for a in L)+s
 print([s,x(2,1)*x(6,2)])
